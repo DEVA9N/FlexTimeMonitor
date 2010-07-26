8d28ad82600a4eee80c785a61ee42044bb2dd1df
aj9226@gmail.com <aj9226@gmail.com@18e5f375-2605-2981-e31c-6f6ab7efd02b>
2010-07-26 22:10:17 +0000

    First checkin

---
+++ trunk/FlexTimeMonitor/FlexTimeMonitor/Options.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace A9N.FlexTimeMonitor
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class WindowOptions : Window
+    {
+        public WindowOptions()
+        {
+            InitializeComponent();
+
+            textBoxWorkPeriod.Text = Properties.Settings.Default.WorkPeriod.ToString();
+            textBoxBreakPeriod.Text = Properties.Settings.Default.BreakPeriod.ToString();
+            textBoxFile.Text = Properties.Settings.Default.LogfileName;
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            if (SaveValues())
+            {
+                Close();
+            }
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private bool SaveValues()
+        {
+            SolidColorBrush validResultBrush = new SolidColorBrush(Colors.Black);
+            SolidColorBrush invalidResultBrush = new SolidColorBrush(Colors.Red);
+            TimeSpan resultWork = TimeSpan.Zero;
+            TimeSpan resultBreak = TimeSpan.Zero;
+
+            bool parseWorkSuccess = TimeSpan.TryParse(textBoxWorkPeriod.Text, out resultWork);
+            bool parseBreakSuccess = TimeSpan.TryParse(textBoxBreakPeriod.Text, out resultBreak);
+            bool parsePathSuccess = System.IO.File.Exists(textBoxFile.Text);
+
+            textBoxWorkPeriod.Foreground = parseWorkSuccess ? validResultBrush : invalidResultBrush;
+            textBoxBreakPeriod.Foreground = parseBreakSuccess ? validResultBrush : invalidResultBrush;
+            textBoxFile.Foreground = parsePathSuccess ? validResultBrush : invalidResultBrush;
+
+            bool allValuesValid = parseWorkSuccess && parseBreakSuccess;// && parsePathSuccess;
+
+            if (allValuesValid)
+            {
+                Properties.Settings.Default.WorkPeriod = resultWork;
+                Properties.Settings.Default.BreakPeriod = resultBreak;
+                Properties.Settings.Default.LogfileName = textBoxFile.Text;
+                Properties.Settings.Default.Save();
+                return true;
+            }
+
+            return false;
+        }
+
+        private void buttonFile_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (System.IO.File.Exists(ofd.FileName))
+                {
+                    textBoxFile.Text = ofd.FileName;
+                    Properties.Settings.Default.LogfileName = ofd.FileName;
+                }
+            }
+        }
+
+    }
+}
---
+++ trunk/FlexTimeMonitor/FlexTimeMonitor/XmlManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace A9N.FlexTimeMonitor
+{
+    /// <summary>
+    /// Manager for generic xml-(de)serialization
+    /// </summary>
+    static class XmlManager
+    {
+        /// <summary>
+        /// Read object of type T from file.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static T Read<T>(String fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                using (XmlReader reader = XmlReader.Create(fileName))
+                {
+                    XmlSerializer x = new XmlSerializer(typeof(T));
+                    return (T)x.Deserialize(reader);
+                }
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Write object to file. No additional type info neccessary.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="output"></param>
+        public static void Write(String fileName, Object output)
+        {
+            StringBuilder builder = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(builder))
+            {
+                XmlSerializer serializer = new XmlSerializer(output.GetType());
+                serializer.Serialize(writer, output);
+
+                XmlDocument outputDocument = new XmlDocument();
+                outputDocument.LoadXml(builder.ToString());
+                outputDocument.Save(fileName);
+            }
+        }
+
+    }
+}
