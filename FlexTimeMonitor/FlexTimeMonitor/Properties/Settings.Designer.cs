d692a381d3c78cb8897802424423064643b37cc2
aj9226 <aj9226@gmail.com>
2014-11-21 12:12:38 +0000

    - added: acquires the settings of previous installed instances

--- FlexTimeMonitor/FlexTimeMonitor/Properties/Settings.Designer.cs
+++ FlexTimeMonitor/FlexTimeMonitor/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace A9N.FlexTimeMonitor.Properties {
                 this["BreakPeriod"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpdateSettings {
+            get {
+                return ((bool)(this["UpdateSettings"]));
+            }
+            set {
+                this["UpdateSettings"] = value;
+            }
+        }
     }
 }
