bbe4a47d99fdc69b1f1e51969c5e981908106429
aj9226@gmail.com <aj9226@gmail.com@18e5f375-2605-2981-e31c-6f6ab7efd02b>
2015-01-27 17:57:43 +0000

    -added controls (deleted by mistake)

---
+++ trunk/FlexTimeMonitor3/FlexTimeMonitor/Controls/DetailViewControls/MonthOverview.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A9N.FlexTimeMonitor.Controls.DetailViewControls
+{
+    /// <summary>
+    /// Interaction logic for MonthOverview.xaml
+    /// </summary>
+    public partial class MonthOverview : UserControl, IDetailView
+    {
+        public IEnumerable<WorkDay> History
+        {
+            get { return this.dataGridWorkDays.ItemsSource as IEnumerable<WorkDay>; }
+            set { this.dataGridWorkDays.ItemsSource = value; }
+        }
+
+        public MonthOverview()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
---
+++ trunk/FlexTimeMonitor3/FlexTimeMonitor/Controls/HistoryTree/TreeItems/MonthTreeItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace A9N.FlexTimeMonitor.Controls.HistoryTree.TreeItems
+{
+    class MonthTreeItem : TreeViewItem
+    {
+        private const double MonthFontSize = 18;
+
+        public IEnumerable<WorkDay> Days { get; private set; }
+
+        public MonthTreeItem(int month, IEnumerable<WorkDay> monthDays)
+        {
+            if (month < 1 || month > 12)
+            {
+                throw new ArgumentException("month");
+            }
+
+            if (monthDays == null)
+            {
+                throw new ArgumentNullException("monthDays");
+            }
+
+            this.Days = monthDays;
+            this.FontSize = MonthFontSize;
+            this.ItemsSource = monthDays;
+            this.Header = GetName(month);
+        }
+
+        public static String GetName(int month)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month);
+        }
+   
+    }
+}
