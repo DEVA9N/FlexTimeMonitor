a921bcdf6a66fad31b817211e2dee2f1a456e7a9
aj9226 <aj9226@gmail.com>
2015-01-27 17:57:43 +0000

    -added controls (deleted by mistake)

---
+++ FlexTimeMonitor3/FlexTimeMonitor/Controls/HistoryTree/TreeItems/IHistoryTreeItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A9N.FlexTimeMonitor.Controls.HistoryTree.TreeItems
+{
+    interface IHistoryTreeItem
+    {
+        int Number { get; }
+
+        String DisplayName { get; }
+
+    }
+}
