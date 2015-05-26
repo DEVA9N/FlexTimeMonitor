c30dccd9510232664cf8b1ddb64fe32947165026
aj9226 <aj9226@gmail.com>
2015-05-26 22:24:01 +0000

    implemented MVVM

---
+++ FlexTimeMonitor/Controls/HistoryTree/TreeItems/MonthViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace A9N.FlexTimeMonitor.Controls.HistoryTree.TreeItems
+{
+    class MonthViewModel : List<WorkDay>
+    {
+        public MonthViewModel(IEnumerable<WorkDay> days)
+        {
+            if (days == null)
+            {
+                throw new ArgumentNullException("days");
+            }
+
+            if (!days.Any())
+            {
+                throw new ArgumentException("days must not be empty");
+            }
+
+            this.Date = days.First().Date;
+
+            this.AddRange(days.ToArray());
+        }
+
+        private DateTime Date { get; set; }
+
+        public String Name { get { return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(this.Date.Month); } }
+    }
+}
