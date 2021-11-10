78de6e14f77d2d2f64ca0da04254206c0262dc3c
DEVA9N <dev@a9n.de>
2021-11-10 16:10:44 +0100

    Move logging initialization and single instance to separate files

--- FlexTimeMonitor/FlexTimeMonitor/App.xaml.cs
+++ FlexTimeMonitor/FlexTimeMonitor/App.xaml.cs
@@ -1,118 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Windows;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
-using System.Threading;
 using System.Threading.Tasks;
-using System.IO;
 using NLog;
+using A9N.FlexTimeMonitor.Facility;
 
-namespace FlexTimeMonitor
+namespace A9N.FlexTimeMonitor
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-
-        [DllImport("user32.dll")]
-        //[return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-
-        private enum SW
-        {
-            /// <summary>
-            /// Hides the window and activates another window.
-            /// </summary>
-            HIDE = 0,
-            /// <summary>
-            /// Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
-            /// </summary>
-            SHOWNORMAL = 1,
-            /// <summary>
-            /// Activates the window and displays it as a minimized window.
-            /// </summary>
-            SHOWMINIMIZED = 2,
-            /// <summary>
-            /// Activates the window and displays it as a maximized window.
-            /// </summary>
-            SHOWMAXIMIZED = 3,
-            /// <summary>
-            /// Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except that the window is not activated.
-            /// </summary>
-            SHOWNOACTIVATE = 4,
-            /// <summary>
-            /// Activates the window and displays it in its current size and position.
-            /// </summary>
-            SHOW = 5,
-            /// <summary>
-            /// Minimizes the specified window and activates the next top-level window in the Z order.
-            /// </summary>
-            MINIMIZE = 6,
-            /// <summary>
-            /// Displays the window as a minimized window. This value is similar to SW_SHOWMINIMIZED, except the window is not activated.
-            /// </summary>
-            SHOWMINNOACTIVE = 7,
-            /// <summary>
-            /// Displays the window in its current size and position. This value is similar to SW_SHOW, except that the window is not activated.
-            /// </summary>
-            SHOWNA = 8,
-            /// <summary>
-            /// Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
-            /// </summary>
-            RESTORE = 9,
-            /// <summary>
-            /// Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application.
-            /// </summary>
-            SHOWDEFAULT = 10,
-            /// <summary>
-            /// Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread.
-            /// </summary>
-            FORCEMINIMIZE = 11,
-        };
-
-        private readonly ILogger _logger;
-        private static Mutex firstInstanceMutex;
-
         public App()
         {
-            EnforceSingleInstance();
+            SingleInstance.EnforceSingleInstance(Current.Shutdown);
 
-            LogManager.Configuration = CreateLoggerConfig();
-            _logger = LogManager.GetLogger(GetType().Name);
+            AppLogging.Inititialize();
 
             // See https://stackoverflow.com/questions/1472498/wpf-global-exception-handler/1472562#1472562
             AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
             Dispatcher.UnhandledException += Dispatcher_UnhandledException;
             TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
-        }
-
-        private static NLog.Config.LoggingConfiguration CreateLoggerConfig()
-        {
-            var logFileName = CreateLogFileName();
-            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = logFileName, Layout = "${longdate}|${level:uppercase=true}|${logger}|${callsite}|${message}" };
-
-            var config = new NLog.Config.LoggingConfiguration();
-            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
-
-            return config;
-        }
-
-        private static string CreateLogFileName()
-        {
-            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var folder = "Flex Time Monitor";
-            var file = "FlexTimeMonitor.log";
-            var logFileName = Path.Combine(appData, folder, file);
-
-            return logFileName;
         }
 
         private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
@@ -141,36 +49,9 @@ namespace FlexTimeMonitor
 
         private void HandleException(Exception e)
         {
-            _logger.Fatal(e);
-        }
+            var logger = LogManager.GetLogger(GetType().Name);
 
-        /// <summary>
-        /// Enforce single instance app
-        /// </summary>
-        static void EnforceSingleInstance()
-        {
-            bool isFirstInstance;
-
-            firstInstanceMutex = new System.Threading.Mutex(true, "FlexTimeMonitorInstanceMutex", out isFirstInstance);
-
-            if (isFirstInstance == false)
-            {
-                Process current = Process.GetCurrentProcess();
-
-                foreach (Process process in Process.GetProcessesByName("FlexTimeMonitor"))//current.ProcessName))
-                {
-                    if (process.Id != current.Id)
-                    {
-                        SetForegroundWindow(process.MainWindowHandle);
-                        ShowWindowAsync(process.MainWindowHandle, (int)SW.SHOW);
-                        ShowWindowAsync(process.MainWindowHandle, (int)SW.RESTORE);
-                        break;
-                    }
-                }
-
-                // Quit this instance
-                Application.Current.Shutdown();
-            }
+            logger.Fatal(e);
         }
     }
 }
---
+++ FlexTimeMonitor/FlexTimeMonitor/Facility/AppLogging.cs
@@ -0,0 +1,39 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A9N.FlexTimeMonitor.Facility
+{
+    internal class AppLogging
+    {
+        internal static void Inititialize()
+        {
+            LogManager.Configuration = CreateLoggerConfig();
+        }
+
+        private static NLog.Config.LoggingConfiguration CreateLoggerConfig()
+        {
+            var logFileName = CreateLogFileName();
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = logFileName, Layout = "${longdate}|${level:uppercase=true}|${logger}|${callsite}|${message}" };
+
+            var config = new NLog.Config.LoggingConfiguration();
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            return config;
+        }
+
+        private static string CreateLogFileName()
+        {
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var folder = "Flex Time Monitor";
+            var file = "FlexTimeMonitor.log";
+            var logFileName = Path.Combine(appData, folder, file);
+
+            return logFileName;
+        }
+    }
+}
