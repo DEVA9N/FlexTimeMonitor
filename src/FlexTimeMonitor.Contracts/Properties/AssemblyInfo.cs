1c11209113d63e399df0736d9516f259d0d5f137
DEVA9N <dev@a9n.de>
2021-11-10 23:56:10 +0100

    Publish 1.3.0.1

--- src/FlexTimeMonitor.Contracts/Properties/AssemblyInfo.cs
+++ src/FlexTimeMonitor.Contracts/Properties/AssemblyInfo.cs
@@ -8,11 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FlexTimeMonitor.Contracts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlexTimeMonitor.Contracts")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -22,15 +19,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6316761c-5836-44a9-8cc1-27178d61016e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/FlexTimeMonitor/Properties/AssemblyInfo.cs
+++ src/FlexTimeMonitor/Properties/AssemblyInfo.cs
@@ -6,22 +6,6 @@
 [assembly: AssemblyTitle("FlexTimeMonitor")]
 [assembly: AssemblyDescription("https://github.com/DEVA9N/FlexTimeMonitor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("A9N")]
 [assembly: AssemblyProduct("FlexTimeMonitor")]
-[assembly: AssemblyCopyright("Copyright © Andre Janßen 2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.*")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+// Shared information will be managed in AssemblyInfoShared.cs
